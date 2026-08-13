f1abcd2c637eb7200b4e2da460e7449cacad3c49
rollinks <rollinks@DESKTOP-1L0D1C1>
2026-08-13 15:37:29 -0400

    begin buffer implementation

--- README.md
+++ README.md
@@ -3,4 +3,16 @@
 
 ## Todo 
 - [ ] drop the crossterm dep and manually set terminal attributes to enter raw or non-canonical mode. C implementation [here](https://viewsourcecode.org/snaptoken/kilo/02.enteringRawMode.html)
+## Plan
+
+Repl should have 3 mode
+  - insert
+  - normal
+  - visual
+
+Each mode has its own keybinding interpretations.
+
+
+---
+
 
---
+++ src/buffer.rs
@@ -0,0 +1,32 @@
+use std::path::{Path, PathBuf};
+use std::{fs, io}
+
+pub struct Buffer {
+    lines: Vec<String>,
+    filename: Option<PathBuf>,
+    modified: bool,
+
+}
+
+impl Buffer {
+    fn empty() -> Self {
+        Buffer {
+            lines: vec![String::new()],
+            filename: None,
+            modified: false
+        }
+    }
+
+    fn from_file<P: AsRef<Path>>(path: P) -> io::Result<Self> {
+        let path = path.as_ref();
+        let contents = fs::read_to_string(path)?;
+        let lines: Vec<String> = contents.lines().map(String::from).collect();
+        let lines = if lines.is_empty() { vec![String::new()] } else { lines };
+
+        Ok(Buffer {
+            lines,
+            filename: Some(path.to_path_buf()),
+            modified: false,
+        })
+    }
+}
